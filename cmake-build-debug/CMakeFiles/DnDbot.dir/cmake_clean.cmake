c352c04c3f8b025b94e12265b161461ef7e17cf2
Башир Арсланалиев <bojik234@gmail.com>
2020-07-15 18:35:00 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/DnDbot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DnDbot.dir/main.cpp.o"
+  "DnDbot"
+  "DnDbot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DnDbot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <string>
+#include <tgbot/tgbot.h>
+
+int main() {
+    srand(time(0));
+    std::ifstream token_file;
+    std::string token;
+    token_file.open("/Users/joelcolbeck/CLionProjects/DnDbot/token_file");
+    if (token_file.is_open())
+    {
+        getline(token_file, token);
+    }
+
+    std::cout << token << std::endl;
+
+    TgBot::Bot bot(token);
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id,
+                "Hello! I'm DnDbot. I can give you random number from 1 to 20. Just type \"/random\"");
+    });
+
+    bot.getEvents().onCommand("random", [&bot](TgBot::Message::Ptr message) {
+        std::string result;
+        result = std::to_string(rand() % 20 + 1);
+        bot.getApi().sendMessage(message->chat->id, "You've got " + result);
+    });
+
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "I don't know, wtf is it!");
+    });
+
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while(true)
+        {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    } catch (TgBot::TgException& e){
+        printf("error: %s\n", e.what());
+    }
+    return 0;
+}
