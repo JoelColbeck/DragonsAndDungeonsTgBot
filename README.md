1544e530ef4ccad5a15f894897a810e0ad10f447
Башир Арсланалиев <bojik234@gmail.com>
2020-07-15 18:41:03 +0300

    Second

---
+++ README.md
@@ -0,0 +1,3 @@
+This bot gives you random number from 1 to 20
+
+Used TgBot library for C++
--- main.cpp
+++ main.cpp
@@ -18,6 +18,14 @@ int main() {
     std::cout << token << std::endl;
 
     TgBot::Bot bot(token);
+
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        if (StringTools::startsWith(message->text, "/start") || StringTools::startsWith(message->text, "/random")) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "I don't know, wtf is it");
+    });
+
     bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
         bot.getApi().sendMessage(message->chat->id,
                 "Hello! I'm DnDbot. I can give you random number from 1 to 20. Just type \"/random\"");
@@ -27,10 +35,6 @@ int main() {
         std::string result;
         result = std::to_string(rand() % 20 + 1);
         bot.getApi().sendMessage(message->chat->id, "You've got " + result);
-    });
-
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "I don't know, wtf is it!");
     });
 
     try {
