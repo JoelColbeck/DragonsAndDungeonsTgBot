f2d594eac1963f9390f512dc2a44d7e28c88f04a
Башир Арсланалиев <bojik234@gmail.com>
2020-07-24 15:52:15 +0300

    Added phrases.h
    Added "/random n" feature, that gives oportunity to get random number from 1 to n
    
    Updated CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@
 #include <fstream>
 #include <string>
 #include <tgbot/tgbot.h>
+#include "phrases.h"
+
+std::string getRandomNumber(std::string messageText) {
+    int number;
+    if (messageText.size() > 7) {
+        number = std::rand() % std::stoi(messageText.substr(8)) + 1;
+    } else {
+        number = std::rand() % 20 + 1;
+        std::cout << number << std::endl;
+    }
+    return std::to_string(number);
+}
 
 int main() {
     srand(time(0));
@@ -27,13 +39,16 @@ int main() {
     });
 
     bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id,
-                "Hello! I'm DnDbot. I can give you random number from 1 to 20. Just type \"/random\"");
+        bot.getApi().sendMessage(message->chat->id, phrase::start);
+//        bot.getApi().sendMessage(message->chat->id,
+//                "Hello! I'm DnDbot. I can give you random number from 1 to n. Just type \"/random n\"");
+//        bot.getApi().sendMessage(message->chat->id,
+//                "If you send only \"/random\", I'll send you random number from 1 to 20");
     });
 
     bot.getEvents().onCommand("random", [&bot](TgBot::Message::Ptr message) {
         std::string result;
-        result = std::to_string(rand() % 20 + 1);
+        result = getRandomNumber(message->text);
         bot.getApi().sendMessage(message->chat->id, "You've got " + result);
     });
 
---
+++ phrases.h
@@ -0,0 +1,6 @@
+#include <string>
+
+
+namespace phrase {
+   std::string start = "Hello! I'm DnDbot. I can give you random number from 1 to n. Just type \"/random n\"\n\nIf you send only \"/random\", I'll send you random number from 1 to 20";
+}
